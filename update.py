3e4f45b076ae3eb06c44e7b134aa5e2b5378af05
yiannisKalt <yiannis.Kaltampanidis@gmail.com>
2019-01-23 13:59:32 +0200

    modified:   update.py

--- update.py
+++ update.py
@@ -6,6 +6,11 @@ from update_kodi_utils import check_for_updates, update_addons
 addon_data_path = '/storage/.kodi/userdata'
 addons_db_path = addon_data_path + '/Database/Addons27.db'
 
+conn = sqlite3.connect(addons_db_path)
+c = conn.cursor()
+c.execute('UPDATE installed SET enabled = 1 WHERE enabled = 0')
+conn.commit()
+conn.close()
 
 # Check if network connection is established. 
 counter = 0
@@ -28,12 +33,7 @@ if check_for_updates():
 
     # Update DB (Enable All Installed Addons)
     # Restart is needed for addons to be enabled.
-    conn = sqlite3.connect(addons_db_path)
-    c = conn.cursor()
-    c.execute('UPDATE installed SET enabled = 1 WHERE enabled = 0')
-    conn.commit()
-    conn.close()
-
+    
     # Restart
     os.system('shutdown -r now')
 
